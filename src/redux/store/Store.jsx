892c4504e0cb5dfb8cf8d7a3dd39c9fdb8c6117c
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-28 16:18:48 +0100

    feat: update Store configuration in redux

--- src/redux/store/Store.jsx
+++ src/redux/store/Store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from '../Slice/auth/auth'
+import eventReducer from '../Slice/event/event'
 
 
 const store = configureStore ({
     reducer: {
         auth: authReducer,
+        event: eventReducer,
       
     }
 });
